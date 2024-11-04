a455bd650c7a4dbc21783f2402f2b6aa758b3ac0
luazars <werbungmaef@gmail.com>
2024-11-04 18:41:52 +0100

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
 
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page v1'),
     );
   }
 }
